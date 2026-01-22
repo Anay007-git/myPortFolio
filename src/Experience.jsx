c1275d646863c9fc0b04de5ade6a90cbe2e35bd6
Anay007-git <98235223+Anay007-git@users.noreply.github.com>
2026-01-22 22:03:27 +0530

    Fix CSS canvas height and add debug visibility test mesh

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -31,5 +31,11 @@ export default function Experience() {
         {/* We always render the car, but we only control it if mode is vehicle */}
         <Car position={[0, 2, -10]} />
 
+        {/* Test Mesh to verify visibility */}
+        <mesh position={[0, 5, 5]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="red" />
+        </mesh>
+
     </>
 }
